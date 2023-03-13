81e8f8cc7b13c4844386640767c5ef0563a222db
Dera <deraadijaya@gmail.com>
2023-03-13 15:49:57 +0700

    komit pertama

---
+++ resources/views/landingpage/index.blade.php
@@ -0,0 +1,168 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Jasa FYP Tiktok</title>
+
+    <meta name="description" content="Layanan jasa FYP tiktok termurah, follower bertambah, view bertambah dan like pun semakin banyak.">
+
+    <meta name="keywords" content="jasa fyp tiktok, jasa fyp, jasa fyp tiktok termurah, jasa follower tiktok, jasa view tiktok, jasa like tiktok">
+
+    <meta name="author" content="Rizal Ahmad">
+
+    <link rel="canonical" href="https://jasafyp.com/"/>
+
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="https://jasafyp.com/"/>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    {{-- css --}}
+    <link rel="stylesheet" href="{{ asset('css/style.css?v=1.1') }}">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap" rel="stylesheet">
+
+    <!-- Favicons -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/logotiktok.png') }}">
+    <link rel="shortcut icon" href="{{ asset('img/logotiktok.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('img/logotiktok.png') }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('img/logotiktok.png') }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('img/logotiktok.png') }}">
+
+  </head>
+
+  <body>
+
+    @include('landingpage.navbar')
+
+    <div class="container-xxl bg-image col-1-konten">
+      <div class="row align-items-start">
+
+        <div class="col">
+          <h1>Jasa FYP TikTok</h1>
+          <p>FYP konten menjadi sebuah capaian keinginan bagi para konten kreator tiktok. Dari FYP tersebut kreator bisa mendapatkan banyak keuntungan, karena itu sangat di harapkan oleh mereka. Adanya jasa FYP tiktok dari kami bisa bantu dorong konten Anda untuk masksimal masuk FYP.</p>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container p-5">
+      <div class="row align-items-start">
+
+        <div class="col">
+          <h2>FYP Tiktok Itu Apa?</h2>
+          <p>FYP adalah sebuah fitur trending yang dimiliki platform tiktok dan berisikan konten - konten sedang viral pada waktu itu. Kepanjangan dari FYP itu sendiri berasal dari bahasa asing yaitu For Your Page yang artinya halaman yang di rekomendasikan untuk Anda.</p>
+        </div>
+
+        <div class="col">
+          <h2>Manfaat FYP</h2>
+          <div class="container text-center">
+          <div class="row">
+            <div class="col-6">Follower Bertambah</div>
+            <div class="col-6">Like Bertambah</div>
+            <div class="col-6">Komentar Bertambah</div>
+            <div class="col-6">Insight Meningkat</div>
+            <div class="col-6">Jangkauan Meluas</div>
+            <div class="col-6">Terkenal</div>
+          </div>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+
+
+    <div class="container-xxl harga">
+      <h2>Harga Jasa <span>FYP TikTok </span>Termurah</h2>
+      <p>Silahkan bandingkan dengan jasa FYP tiktok lain harga yang mana termurah? Sudah pasti kami yang paling murah dengan banyak kelebihan!</p>
+      <div class="row">
+
+        <div class="col-harga">
+          <h3>Entry</h3>
+          <p class="nominal">Rp <span>750.000</span></p>
+          <a href="/" class="link-no-dekor">
+            <div class="d-grid">
+              <button class="btn btn-secondary btn-lg" type="button">Pesan Sekarang</button>
+            </div>
+          </a>
+          <ul class="link-no-dekor fitur">
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">Jangkau 500++ User</li>
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">Push 50++ Komentar</li>
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">Push 100++ Like</li>
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">Untuk 1 Konten</li>
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">Analisis Pemirsa Indonesia</li>
+            <li class="coret fitur-list"><img class="fitur-list" src="{{ asset('icon/x-square.svg') }}" style="color:white;">Share Story</li>
+          </ul>
+        </div>
+
+        <div class="col-harga">
+          <h3>Small</h3>
+          <p class="nominal">Rp <span>1,2 Jt</span></p>
+          <a href="/" class="link-no-dekor">
+            <div class="d-grid">
+              <button class="btn btn-secondary btn-lg" type="button">Pesan Sekarang</button>
+            </div>
+          </a>
+          <ul class="link-no-dekor fitur">
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">Jangkau 1000++ User</li>
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">Push 50++ Komentar</li>
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">Push 200++ Like</li>
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">Untuk 1 Konten</li>
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">Analisis Pemirsa Indonesia</li>
+            <li class="coret fitur-list"><img class="fitur-list" src="{{ asset('icon/x-square.svg') }}" style="color:white;">Share Story</li>
+          </ul>
+        </div>
+
+        <div class="col-harga medium">
+          <h3>Medium</h3>
+          <p class="nominal">Rp <span>3,5 Jt</span></p>
+          <a href="/" class="link-no-dekor">
+            <div class="d-grid">
+              <button class="btn btn-primary btn-lg" type="button">Pesan Sekarang</button>
+            </div>
+          </a>
+          <ul class="link-no-dekor fitur">
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">Jangkau 3000++ User</li>
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">Push 100++ Komentar</li>
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">Push 1000++ Like</li>
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">Untuk 2 Konten</li>
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">Analisis Pemirsa Indonesia</li>
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">100 Share Story</li>  
+          </ul>
+        </div>
+
+        <div class="col-harga normal">
+          <h3>Large</h3>
+          <p class="nominal">Rp <span>5,5 Jt</span></p>
+          <a href="/" class="link-no-dekor">
+            <div class="d-grid">
+              <button class="btn btn-secondary btn-lg" type="button">Pesan Sekarang</button>
+            </div>
+          </a>
+          <ul class="link-no-dekor fitur">
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">Jangkau 10.000++ User</li>
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">Push 200++ Komentar</li>
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">Push 5000++ Like</li>
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">Untuk 1 Konten</li>
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">Analisis Pemirsa Indonesia</li>
+            <li class="fitur-list"><img class="fitur-list" src="{{ asset('icon/check-square.svg') }}" style="color:white;">200 Share Story</li>  
+          </ul>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="container">
+
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    
+  </body>
+
+
+</html>
